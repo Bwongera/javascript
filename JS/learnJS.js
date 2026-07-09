344fec7fecdcb73f01cfdd6d598220b6cc04b2c2
Bwongera <johnngabonziza9@gmail.com>
2026-07-09 17:03:37 +0200

    javascript code first

--- JS/learnJS.js
+++ JS/learnJS.js
@@ -1,3 +1,5 @@
+
+
 const message = "HELLO WORLD";
 console.log(message);
 
@@ -11,19 +13,47 @@ const information = ["car", true, 50, Date("09-07-2026"), "banana"];
 
 console.log(information);
 
-const product = 
-  {name: "Laptop",
-  price: 1000000,
-  available: true}
+const product =
+{
+    name: "Laptop",
+    price: 1000000,
+    available: true
+}
 
 
 console.log(product);
 
 const people = [
 
-    {name: "Ishimwe", age: 30 },
-    {name: "Jeanne", age: 25 },
-    {name: "Claude", age: 27 }
+    { name: "Ishimwe", age: 30 },
+    { name: "Jeanne", age: 25 },
+    { name: "Claude", age: 27 }
 ];
 console.log(people);
+
+
+
+const families = [
+    {
+        name: "Ngabo",
+        gender: "Male",
+        married: false,
+        location: { country: "Rwanda", city: "Kigali", district: "Gasabo" },
+        children: [
+            { name: "Kevin", age: 10, gender: "Male" },
+            { name: "Yvan", age: 8, gender: "Male" }
+        ]
+    },
+    {
+        name: "Sarah",
+        gender: "Female",
+        married: true,
+        location: { country: "Kenya", city: "Nairobi", district: "Westlands" },
+        children: [
+            { name: "Emma", age: 6, gender: "Female" }
+        ]
+    }
+];
+
+console.log(families);
 
