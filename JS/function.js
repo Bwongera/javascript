f3c3e98ada6969e1aa0f42d334a70d6fd3ea4ace
Bwongera <johnngabonziza9@gmail.com>
2026-07-20 16:28:43 +0200

    third comment in js

--- JS/function.js
+++ JS/function.js
@@ -1,28 +1,55 @@
 function GreetingMessage() {
     return "Hello my baby boy"
-   
+
 }
 console.log(GreetingMessage());
 
-function addElement(){
-    const number=[ 1,2,3,4,5];
+function addElement() {
+    const number = [1, 2, 3, 4, 5];
     number.push(6)
     return number;
 
 }
 console.log(addElement())
 
-function greetUser(){
+function greetUser() {
     return "Hello, John ?"
 }
 
 console.log(greetUser())
 
 
-function greetUsers(){
+function greetUsers() {
     const personalName = 'Ngabo'
     const sentence = `Hello ${personalName}?`
     return sentence
 
 }
- console.log(greetUsers())
+console.log(greetUsers())
+
+function multiply() {
+    const a = 4
+    const b = 5
+    return a * b;
+
+}
+console.log(multiply())
+
+function multiplys(a, b, c) {
+    return a * b / c;
+}
+console.log(multiplys(4, 5, 2))
+
+
+function userData(name,email,phone,id) {
+    const user = {
+        Names:name,
+        Email:email,
+        PhoneNumber:phone,
+        ID:id
+    }
+    return user
+
+}
+console.log(userData("John Ngabonziza", "John@gmail.com", "0782512832", 21991))
+
