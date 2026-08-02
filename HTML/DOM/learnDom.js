c420a438d171f13b372f5905e8ea49ccc7f89f65
Bwongera <johnngabonziza9@gmail.com>
2026-08-02 17:03:46 +0200

    comment from dom

---
+++ HTML/DOM/learnDom.js
@@ -0,0 +1,40 @@
+// let hello = document.querySelector('h1')
+
+// let title = document.getElementsByClassName('title')
+// let subtitle = document.getElementById('sub-title')
+// let description = document.getElementsByTagName('p')
+
+
+let Title = document.querySelector('.title');
+Title.innerHTML = 'Sport News';
+Title.style.color = 'red'
+
+let subTitle = document.querySelector("#sub-title");
+subTitle.innerHTML = "Cecafa Kagame Cup";
+
+let paragraph = document.querySelector('p');
+paragraph.innerHTML = 'The CECAFA Kagame Cup is an annual regional club football tournament organized by the Council for East and Central Africa Football Association (CECAFA). It features top club sides from East and Central Africa and has been sponsored by Rwandan President Paul Kagame since 2002,Tournament OverviewWhat it is: A regional championship for club football teams from member nations like Rwanda, Uganda, Tanzania, Kenya, and Sudan.History: Founded earlier as the CECAFA Club Cup, it was renamed the Kagame Interclub Cup in 2002 when President Paul Kagame started financial sponsorship.Current Event: The 2026 CECAFA Kagame Cup is currently taking place in Kigali, Rwanda, running from July 24 to August 7, 2026, with matches hosted at venues like the Amahoro Stadium.Key FactsParticipants: Features 12 top club teams divided into group stages, including local sides like Rayon Sports and APR FC, alongside regional heavyweights.Prizes: The tournament offers cash prizes supported by the Rwandan presidency, with the champion traditionally receiving $30,000.';
+
+let button = document.querySelector('button')
+let result = document.querySelector('span')
+button.addEventListener('click', function(){
+result.innerHTML = "We don't have other more description"
+}
+)
+
+let on = document.querySelector("#on-image")
+on.style.display = "none"
+let off = document.querySelector("#off-image")
+
+let onbutton = document.querySelector(".on")
+let offbutton = document.querySelector(".off")
+
+onbutton.addEventListener("click", function(){
+    on.style.display = "block"
+    off.style.display = "none"
+})
+
+offbutton.addEventListener("click",function(){
+    on.style.display = "none"
+    off.style.display = "block"
+})
--- JS/qNine.js
+++ JS/qNine.js
@@ -1,134 +1,134 @@
-let reword = [ 'Hey', "I'm", 'John', 'a', 'good', 'student.']
-let sentences = reword.join(" ")
-console.log(sentences)
+// let reword = [ 'Hey', "I'm", 'John', 'a', 'good', 'student.']
+// let sentences = reword.join(" ")
+// console.log(sentences)
 
-let women = [ 'Gisele', 'Clemence','Manzere','Judith']
-console.log(women)
+// let women = [ 'Gisele', 'Clemence','Manzere','Judith']
+// console.log(women)
+
+// // console.log(women.length)
+
+// women.push('Donatha')
+
+// women.unshift()
+// console.log(women)
+
+// women.pop()
+// console.log(women)
+
+// women.shift()
+// console.log(women)
+
+// console.log(women.join(" , "))
 
 // console.log(women.length)
 
-women.push('Donatha')
 
-women.unshift()
-console.log(women)
-
-women.pop()
-console.log(women)
-
-women.shift()
-console.log(women)
-
-console.log(women.join(" , "))
-
-console.log(women.length)
+// const Seat = [
+//   'reserved',
+//   'reserved',
+//   'occupied',
+//   'occupied',
+//   'available',
+//   'available'
+// ]
 
 
-const Seat = [
-  'reserved',
-  'reserved',
-  'occupied',
-  'occupied',
-  'available',
-  'available'
-]
+// for (let seat = 1; seat <= 6; seat++) {
+//     if (seat <= 2) {
+//         console.log(`Seat ${seat} = Reserved`);
+//     } else if (seat <= 4) {
+//         console.log(`Seat ${seat} = Occupied`);
+//     } else {
+//         console.log(`Seat ${seat} = Available`);
+//     }
+// }
 
+// const seats = [
+//   { owner: "MANZI", price: '$100', category: "VVIP", status: "OCCUPIED" },
+//   { owner: "MUTONI", price: '$25', category: "REGULAR", status: "RESERVED" },
+//   { owner: "EGIDE", price: '$50', category: "VIP", status: "RESERVED" },
+//   { owner: null, price: '$100', category: "VVIP", status: "AVAILABLE" },
+//   { owner: "JUSTIN", price: '$25', category: "REGULAR", status: "OCCUPIED" },
+// ];
 
-for (let seat = 1; seat <= 6; seat++) {
-    if (seat <= 2) {
-        console.log(`Seat ${seat} = Reserved`);
-    } else if (seat <= 4) {
-        console.log(`Seat ${seat} = Occupied`);
-    } else {
-        console.log(`Seat ${seat} = Available`);
-    }
-}
+// let personMessage;
+// let seatMessage;
 
-const seats = [
-  { owner: "MANZI", price: '$100', category: "VVIP", status: "OCCUPIED" },
-  { owner: "MUTONI", price: '$25', category: "REGULAR", status: "RESERVED" },
-  { owner: "EGIDE", price: '$50', category: "VIP", status: "RESERVED" },
-  { owner: null, price: '$100', category: "VVIP", status: "AVAILABLE" },
-  { owner: "JUSTIN", price: '$25', category: "REGULAR", status: "OCCUPIED" },
-];
+// seats.forEach((seat, index) => {
+//   if (seat.owner === 'MANZI') {
+//     personMessage = `Hey Jose, ${seat.owner} already entered and his in ${seat.category} of ${seat.price}.`
+//   }
+//   if (seat.status === "AVAILABLE") {
+//     seatMessage = `Please check the seat ${index + 1} is ${seat.status}.`
+//   }
 
-let personMessage;
-let seatMessage;
+// })
 
-seats.forEach((seat, index) => {
-  if (seat.owner === 'MANZI') {
-    personMessage = `Hey Jose, ${seat.owner} already entered and his in ${seat.category} of ${seat.price}.`
-  }
-  if (seat.status === "AVAILABLE") {
-    seatMessage = `Please check the seat ${index + 1} is ${seat.status}.`
-  }
-
-})
-
-console.log(`${personMessage} ${seatMessage}`)
+// console.log(`${personMessage} ${seatMessage}`)
 
 
 
-students = [
-    { name: 'Karinda', age: 24, marks: 73, grade: 'B' },
-    { name: 'Grolia', age: 17, marks: 60, grade: 'C' },
-    { name: 'Leah', age: 23, marks: 90, grade: 'A' },
-    { name: 'Mpano', age: 20, marks: 40, grade: 'D' }
-];
+// students = [
+//     { name: 'Karinda', age: 24, marks: 73, grade: 'B' },
+//     { name: 'Grolia', age: 17, marks: 60, grade: 'C' },
+//     { name: 'Leah', age: 23, marks: 90, grade: 'A' },
+//     { name: 'Mpano', age: 20, marks: 40, grade: 'D' }
+// ];
 
-students.forEach((student, index) =>{
-    if (student.marks >= 70) {
-        console.log(`${student.name} ${student.marks}: EXELLENCE`)
-    } else if(student.marks >=50 && student.marks < 70){
-        console.log(`${student.name} ${student.marks}: GOOD`)
-    }else{
-        console.log(`${student.name} ${student.marks}: FAIL`)
-    }
-    })
+// students.forEach((student, index) =>{
+//     if (student.marks >= 70) {
+//         console.log(`${student.name} ${student.marks}: EXELLENCE`)
+//     } else if(student.marks >=50 && student.marks < 70){
+//         console.log(`${student.name} ${student.marks}: GOOD`)
+//     }else{
+//         console.log(`${student.name} ${student.marks}: FAIL`)
+//     }
+//     })
 
-    for( let x = 0; x < students.length; x++){
-console.log(`The student name is ${students[x].name}: they have ${students[x].marks} marks.`)
+//     for( let x = 0; x < students.length; x++){
+// console.log(`The student name is ${students[x].name}: they have ${students[x].marks} marks.`)
 
-    }
+//     }
 
-    function removingTheExclamation(Word){
-        let newWord = "";
-        for(let i = 0; i < Word.length; i++){
-            if(Word[i] !== "!")
-            newWord += Word[i];
-        }
-        return  newWord;
-    }
-    console.log(removingTheExclamation('Suprise'))
+//     function removingTheExclamation(Word){
+//         let newWord = "";
+//         for(let i = 0; i < Word.length; i++){
+//             if(Word[i] !== "!")
+//             newWord += Word[i];
+//         }
+//         return  newWord;
+//     }
+//     console.log(removingTheExclamation('Suprise'))
 
-let attendance = [
-    'Present',
-    'Absent',
-    'Present',
-    'Absent',
-    'Present',
-    'Absent',
-    'Present',
-    'Absent',
-    'Present',
-    'Absent'
-];
-let totalStudent = 0;
-let PresentStudent = 0;
-let AbsentStudent = 0;
+// let attendance = [
+//     'Present',
+//     'Absent',
+//     'Present',
+//     'Absent',
+//     'Present',
+//     'Absent',
+//     'Present',
+//     'Absent',
+//     'Present',
+//     'Absent'
+// ];
+// let totalStudent = 0;
+// let PresentStudent = 0;
+// let AbsentStudent = 0;
 
-while (totalStudent < 10) {
-    let status = prompt('please enter present or absent')
-    if (status === 'Present') {
-        PresentStudent++
-        totalStudent++
-    } else if (status === 'Absent') {
-        AbsentStudent++
-        totalStudent++
-    } else {
-        console.log('please enter present or absent')
-    }
-}
+// while (totalStudent < 10) {
+//     let status = prompt('please enter present or absent')
+//     if (status === 'Present') {
+//         PresentStudent++
+//         totalStudent++
+//     } else if (status === 'Absent') {
+//         AbsentStudent++
+//         totalStudent++
+//     } else {
+//         console.log('please enter present or absent')
+//     }
+// }
 
-console.log('total students:', totalStudent);
-console.log('Present students:', PresentStudent);
-console.log('Absent students:', AbsentStudent);
+// console.log('total students:', totalStudent);
+// console.log('Present students:', PresentStudent);
+// console.log('Absent students:', AbsentStudent);
