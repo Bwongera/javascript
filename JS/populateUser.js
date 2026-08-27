193b8d1f733545a5adc611309d7680811f51f95b
Bwongera <johnngabonziza9@gmail.com>
2026-08-27 10:15:35 +0200

    To changes js populater user code

--- JS/populateUser.js
+++ JS/populateUser.js
@@ -1,180 +1,118 @@
 
 const tableBody = document.querySelector("tbody")
+let userId = undefined;
 
 async function fetchUsers() {
-try {
-   const response = await fetch("https://fakestoreapi.com/users") 
-   const users = await response.json()
+   try {
+      const response = await fetch("https://fakestoreapi.com/users")
+      const users = await response.json()
 
-users.map((user,index)=>{
-const tableRow = document.createElement('tr')
-tableRow.innerHTML = `
-<td>${index +1}</td>
+      users.map((user) => {
+         const tableRow = document.createElement('tr')
+         tableRow.setAttribute("data-user-id", user.id)
+
+         tableRow.innerHTML = `
+<td>${user.id}</td>
 <td>${user.name.firstname}</td>
 <td>${user.name.lastname}</td>
 <td>${user.email}</td>
 <td>${user.username}</td>
 <td>${user.address.city}</td>
-
 <button onclick='updateUser(${JSON.stringify(user)})'><i class="fa-solid fa-pen" style="color: rgb(0, 255, 0);"></i></button>
 <button onclick=(deleteUser(${user.id}))><i class="fa-solid fa-trash" style="color: rgb(255, 0, 0);"></i></button>
 
 `
-tableBody.appendChild(tableRow)
-})
-  }  catch(error)  {
-        console.log(`Error is :${error}`)
-    }
+         tableBody.appendChild(tableRow)
+      })
+   } catch (error) {
+      console.log(`Error is :${error}`)
+   }
 
 }
-   fetchUsers()
 
- async function updateUser(user){
+fetchUsers()
 
-      console.log('Default user:',user)
+const updateUserForm = document.createElement("form")
+updateUserForm.id = "updateModal";
+updateUserForm.style.display = 'none'
 
-      const updateUserForm = document.createElement("form")
-      updateUserForm.innerHTML = `
+updateUserForm.innerHTML = `
       <h1 >Update User</h1>
-      <input class='form-input' type='text' value=${user.name.firstname} />
-      <input class='form-input' type='text' value=${user.name.lastname} />
-      <input class='form-input' type='text' value=${user.username} />
-      <input class='form-input' type='text' value=${user.address.city} />
+      <input class='form-input' id='firstName' type='text' />
+      <input class='form-input' id='lastName' type='text' />
+      <input class='form-input' id='userName' type='text' />
+      <input class='form-input' id='address' type='text'  />
 
       <div>
       <button class='form-button' style="background: red;">Cancel</button>
-      <button class='form-button' style="background: blue;">Update</button>
+      <button id='submit-button' class='form-button' style="background: blue;">Update</button>
       </div>
 
       `
 
-      document.body.appendChild(updateUserForm)
+document.body.appendChild(updateUserForm)
+
+const submitButton = document.querySelector("#submit-button")
+const firstName = document.querySelector("#firstName")
+const lastName = document.querySelector("#lastName")
+const userName = document.querySelector("#userName")
+const address = document.querySelector("#address")
+
+const updateUser = (user) => {
+   updateUserForm.style.display = 'block'
+   firstName.value = user.name.firstname
+   lastName.value = user.name.lastname
+   userName.value = user.username
+   address.value = user.address.city
+   userId = user.id
+}
+
+const submitForm = async () => {
+   const data = {
+      name: { firstname: firstName.value, lastname: lastName.value },
+      username: userName.value,
+      address: { city: address.value }
+
    }
+   try {
+      const response = await fetch(`https://fakestoreapi.com/users/${userId}`, { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) })
+      const updateUser = await response.json()
+      console.log(updateUser);
+
+      const tableRow = document.querySelector(`tr[data-user-id="${userId}"]`);
+      tableRow.children[1].textContent = firstName.value
+      tableRow.children[2].textContent = lastName.value
+      tableRow.children[4].textContent = userName.value
+      tableRow.children[5].textContent = address.value
+      updateUserForm.style.display = 'none'
+
+   } catch (error) {
+      console.log(`Error is:${error}`)
+   }
+}
+
+submitButton.addEventListener("click", function (event) {
+   event.preventDefault();
+   submitForm();
+
+}
+)
+
+const deleteUser = async (userId) => {
+   console.log(userId)
+
+   try {
+      const response = await fetch(`https://fakestoreapi.com/users/${userId}`, { method: "DELETE" })
+      const deleteUser = await response.json()
+      console.log(deleteUser);
+
+      document.querySelector(`tr[data-user-id="${userId}"]`).remove();
+      alert('User deleted successfuly.');
+
+   } catch (error) {
+      console.log(`Error is: ${error}`)
+   }
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const tableBody = document.querySelector("tbody");
-
-// async function fetchUsers() {
-//   try {
-//     const response = await fetch("https://fakestoreapi.com/users");
-//     const users = await response.json();
-
-//     users.map((user, index) => {
-//       const tableRow = document.createElement("tr");
-
-//       tableRow.innerHTML = `
-//         <td>${index + 1}</td>
-//         <td>${user.name.firstname}</td>
-//         <td>${user.name.lastname}</td>
-//         <td>${user.email}</td>
-//         <td>${user.username}</td>
-//         <td>${user.address.city}</td>
-
-//         <td><button class="edit-btn"><i class="fa-solid fa-pen" style="color: rgb(0, 255, 0);"></i></button>
-//           <button class="delete-btn"><i class="fa-solid fa-trash" style="color: rgb(255, 0, 0);"></i></button>
-//         </td>
-//       `;
-
-//       const editButton = tableRow.querySelector(".edit-btn");
-//       editButton.addEventListener("click", () => {
-//         updateUser(user);
-//       });
-//       const deleteButton = tableRow.querySelector(".delete-btn");
-
-//       deleteButton.addEventListener("click", () => {
-//         tableRow.remove();
-//       });
-
-//       tableBody.appendChild(tableRow);
-//     });
-
-//   } catch (error) {
-//     console.log(`Error is: ${error}`);
-//   }
-// }
-
-// fetchUsers();
-
-
-// function updateUser(user) {
-
-//   console.log("Default user:", user);
-
-//   const updateUserForm = document.createElement("form");
-
-//   updateUserForm.innerHTML = `
-//     <h1>Update User</h1>
-
-//     <input class="form-input" type="text"  value="${user.name.firstname}"  placeholder="First Name"/>
-//     <input  class="form-input"  type="text"  value="${user.name.lastname}"  placeholder="Last Name"/>
-//     <input class="form-input" type="text" value="${user.username}" placeholder="Username"/>
-//     <input class="form-input" type="text" value="${user.address.city}"  placeholder="City"/>
-
-//     <div>
-//       <button type="button" class="form-button cancel-btn">Cancel</button>
-//       <button type="submit" class="form-button">Update</button>
-//     </div>
-//   `;
-
-//   document.body.appendChild(updateUserForm);
-
-//   const cancelButton = updateUserForm.querySelector(".cancel-btn");
-
-//   cancelButton.addEventListener("click", () => {
-//     updateUserForm.remove();
-//   });
-
-//   updateUserForm.addEventListener("submit", (event) => {
-//     event.preventDefault();
-
-//     const inputs = updateUserForm.querySelectorAll(".form-input");
-
-//     user.name.firstname = inputs[0].value;
-//     user.name.lastname = inputs[1].value;
-//     user.username = inputs[2].value;
-//     user.address.city = inputs[3].value;
-
-//     console.log("Updated user:", user);
-
-//     updateUserForm.remove();
-//   });
-// }
-
-   
-
-  
